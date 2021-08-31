5ed6d5e85b01352be69a6cb602577d483852b024
Abhijeet452 <iit2020214@iiita.ac.in>
2021-09-01 02:38:00 +0530

    Added some more functionalities

--- dashboardjs.js
+++ dashboardjs.js
@@ -72,13 +72,17 @@ async function charttotalcases() {
             },
             layout: {
                 padding: {
-                    left: 30,
-                    right: 30
+                    left: 40,
+                    right: 40,
+                    bottom: 20,
+                    top: 20
                 }
             }
         }
     });
 }
+
+chartIt30total();
 async function chartIt30total() {
     const ctx =
         document.getElementById('Last30Days').getContext('2d');
@@ -94,7 +98,7 @@ async function chartIt30total() {
         data: {
             labels: taarik30,
             datasets: [{
-                label: 'Daily_Cases_Confirmed',
+                label: 'Cases/Day Of Last 30 Days',
                 data: number30,
                 backgroundColor: 'orange',
                 borderColor: 'orange',
@@ -111,90 +115,175 @@ async function chartIt30total() {
             },
             layout: {
                 padding: {
-                    left: 30,
-                    right: 30
+                    left: 40,
+                    right: 40,
+                    bottom: 20,
+                    top: 20
                 }
             }
         }
     });
 }
-chartIt30total();
-// charttotalcasesrecoved();
-// async function charttotalcasesrecoved() {
-//     var ctx = document.getElementById('myChartrecovered').getContext('2d');
-//     Chart.defaults.font.size = 12;
-//     let data = await getData();
-//     const myChart = new Chart(ctx, {
-//         type: 'bar',
-//         data: {
-//             labels: data.Xdates,
-//             datasets: [{
-//                 label: 'Recovered Cases/Day',
-//                 data: data.RecoveredCases,
-//                 backgroundColor: 'green',
-//                 borderColor: 'green',
-//                 borderWidth: 3
-//             }]
-//         },
-//         options: {
-//             responsive: true,
-//             maintainAspectRatio: true,
-//             scales: {
-//                 y: {
-//                     beginAtZero: true
-//                 }
-//             },
-//             layout: {
-//                 padding: {
-//                     left: 30,
-//                     right: 30
-//                 }
-//             }
-//         }
-//     });
-// }
-// chartIt30total();
-// async function chartIt30total() {
-//     const ctx =
-//         document.getElementById('Last30Daysrecovored').getContext('2d');
-//     const data = await getData();
-//     let taarik30 = [];
-//     let number30 = [];
-//     for (var i = 0; i < 30; i++) {
-//         taarik30.push(data.Xdates[data.Xdates.length - 30 + i]);
-//         number30.push(data.RecoveredCases[data.RecoveredCases.length - 30 + i]);
-//     }
-//     const myChart = new Chart(ctx, {
-//         type: 'line',
-//         data: {
-//             labels: taarik30,
-//             datasets: [{
-//                 label: 'Daily_Cases_Recovered',
-//                 data: number30,
-//                 backgroundColor: 'green',
-//                 borderColor: 'green',
-//                 borderWidth: 1
-//             }]
-//         },
-//         options: {
-//             maintainAspectRatio: true,
-//             responsive: true,
-//             scales: {
-//                 y: {
-//                     beginAtZero: false
-//                 }
-//             },
-//             layout: {
-//                 padding: {
-//                     left: 30,
-//                     right: 30
-//                 }
-//             }
-//         }
-//     });
-// }
+charttotalcasesrecoved();
+async function charttotalcasesrecoved() {
+    var ctx = document.getElementById('myChartrecovered').getContext('2d');
+    Chart.defaults.font.size = 12;
+    let data = await getData();
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: data.Xdates,
+            datasets: [{
+                label: 'Recovered Cases/Day',
+                data: data.RecoveredCases,
+                backgroundColor: 'green',
+                borderColor: 'green',
+                borderWidth: 3
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            layout: {
+                padding: {
+                    left: 40,
+                    right: 40,
+                    bottom: 20,
+                    top: 20
+                }
+            }
+        }
+    });
+}
+chartIt30totalrecovered();
+async function chartIt30totalrecovered() {
+    const ctx =
+        document.getElementById('Last30Daysrecovored').getContext('2d');
+    const data = await getData();
+    let taarik30 = [];
+    let number30 = [];
+    for (var i = 0; i < 30; i++) {
+        taarik30.push(data.Xdates[data.Xdates.length - 30 + i]);
+        number30.push(data.RecoveredCases[data.RecoveredCases.length - 30 + i]);
+    }
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: taarik30,
+            datasets: [{
+                label: 'Recovered Cases/Day Of Last 30 Days',
+                data: number30,
+                backgroundColor: 'green',
+                borderColor: 'green',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: true,
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            },
+            layout: {
+                padding: {
+                    left: 40,
+                    right: 40,
+                    bottom: 20,
+                    top: 20
+                }
+            }
+        }
+    });
+}
+charttotalcasesdeceased();
+async function charttotalcasesdeceased() {
+    var ctx = document.getElementById('myChartdeceased').getContext('2d');
+    Chart.defaults.font.size = 12;
+    let data = await getData();
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: data.Xdates,
+            datasets: [{
+                label: 'Deceased Cases/Day',
+                data: data.DeceasedCases,
+                backgroundColor: 'red',
+                borderColor: 'red',
+                borderWidth: 3
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            layout: {
+                padding: {
+                    left: 40,
+                    right: 40,
+                    bottom: 20,
+                    top: 20
+                }
+            }
+        }
+    });
+}
+chartIt30totaldeceased();
+async function chartIt30totaldeceased() {
+    const ctx =
+        document.getElementById('Last30Daysdeceased').getContext('2d');
+    const data = await getData();
+    let taarik30 = [];
+    let number30 = [];
+    for (var i = 0; i < 30; i++) {
+        taarik30.push(data.Xdates[data.Xdates.length - 30 + i]);
+        number30.push(data.DeceasedCases[data.DeceasedCases.length - 30 + i]);
+    }
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: taarik30,
+            datasets: [{
+                label: 'Deceased Cases/Day Of Last 30 Days',
+                data: number30,
+                backgroundColor: 'red',
+                borderColor: 'red',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: true,
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            },
+            layout: {
+                padding: {
+                    left: 40,
+                    right: 40,
+                    bottom: 20,
+                    top: 20
+                }
+            }
+        }
+    });
+}
 window.onload = function graph30display() {
     document.getElementById('Last30Days').style.display = 'none';
+    document.getElementById('Last30Daysrecovored').style.display = 'none';
+    document.getElementById('Last30Daysdeceased').style.display = 'none';
 }
 
 function changetotalgraph() {
@@ -202,7 +291,29 @@ function changetotalgraph() {
     document.getElementById('Last30Days').style.display = "none";
 }
 
+function changetotalgraphrecovered() {
+    document.getElementById('myChartrecovered').style.display = "block";
+    document.getElementById('Last30Daysrecovored').style.display = "none";
+}
+
+function changetotalgraphdeceased() {
+    document.getElementById('myChartdeceased').style.display = "block";
+    document.getElementById('Last30Daysdeceased').style.display = "none";
+}
+
+
 function change30graph() {
     document.getElementById('myChart').style.display = "none";
     document.getElementById('Last30Days').style.display = "block";
+
+}
+
+function change30graphrecovered() {
+    document.getElementById('myChartrecovered').style.display = "none";
+    document.getElementById('Last30Daysrecovored').style.display = "block";
+}
+
+function change30graphdeceased() {
+    document.getElementById('myChartdeceased').style.display = "none";
+    document.getElementById('Last30Daysdeceased').style.display = "block";
 }
